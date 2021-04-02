d615c84eef6c0c92c2892dc5a4cbf48f79d34148
claudioge <claudio.gebbia@hotmail.com>
2021-04-02 16:16:35 +0200

    Update Header.js

--- src/components/gamepage/Header.js
+++ src/components/gamepage/Header.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Button, Form } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Dropdown, Menu } from 'semantic-ui-react'
 
 class Header extends React.Component{
 
+    state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu size='small' inverted>
+        <Menu.Item>
+          <img src='https://react.semantic-ui.com/logo.png' alt ={NaN}/>
+        </Menu.Item>
+        <Menu.Item
+          name='Score'
+          active={activeItem === 'Score'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='Time'
+          active={activeItem === 'Time'}
+          onClick={this.handleItemClick}
+        />
 
 
-
-    render(){
-        return;
-    }
-
+          <Menu.Item position ='right'>
+            <Button color='red'>exit</Button>
+          </Menu.Item>
+      </Menu>
+    )
+  }
 
 }
+
 export default Header;
