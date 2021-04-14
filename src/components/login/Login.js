02c4895efa3261be9ca3489d598537e75c396a2d
Jerome Hadorn <jerome.hadorn@corestream.co.uk>
2021-04-14 15:21:56 +0200

    Added linter & convenience method for making API requests

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
-import { BaseContainer } from '../../helpers/layout';
-import { api, handleError } from '../../helpers/api';
-import User from '../shared/models/User';
-import { withRouter } from 'react-router-dom';
-import { Button, Form, Label, Input } from 'semantic-ui-react'
+import React from "react";
+import styled from "styled-components";
+import { BaseContainer } from "../../helpers/layout";
+import { api, handleError } from "../../helpers/api";
+import User from "../shared/models/User";
+import { withRouter } from "react-router-dom";
+import { Button, Form, Label, Input } from "semantic-ui-react";
 
 const FormContainer = styled.div`
   margin-top: 2em;
@@ -14,87 +14,55 @@ const FormContainer = styled.div`
   justify-content: center;
 `;
 
-
-
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 20px;
 `;
 
-/**
- * Classes in React allow you to have an internal state within the class and to have the React life-cycle for your component.
- * You should have a class (instead of a functional component) when:
- * - You need an internal state that cannot be achieved via props from other parent components
- * - You fetch data from the server (e.g., in componentDidMount())
- * - You want to access the DOM via Refs
- * https://reactjs.org/docs/react-component.html
- * @Class
- */
 class Login extends React.Component {
-  /**
-   * If you don’t initialize the state and you don’t bind methods, you don’t need to implement a constructor for your React component.
-   * The constructor for a React component is called before it is mounted (rendered).
-   * In this case the initial state is defined in the constructor. The state is a JS object containing two fields: name and username
-   * These fields are then handled in the onChange() methods in the resp. Inputs
-   */
   constructor() {
     super();
     this.state = {
       username: null,
-      password:null
+      password: null,
     };
   }
-  /**
-   * HTTP POST request is sent to the backend.
-   * If the request is successful, a new user is returned to the front-end
-   * and its token is stored in the localStorage.
-   */
+
   async login() {
     try {
       const requestBody = JSON.stringify({
         username: this.state.username,
-        password: this.state.password
+        password: this.state.password,
       });
-      const response = await api.post('/login', requestBody);
+      const response = await api.post("/login", requestBody);
 
       // Get the returned user and update a new object.
       const user = new User(response.data);
 
       // Store the token into the local storage.
-      localStorage.setItem('token', user.token);
-      localStorage.setItem('currentUserId', /*user.id*/"10");
+      localStorage.setItem("token", user.token);
+      //TODO: user the correct user.id here and not just the defaulting to 10
+      localStorage.setItem("currentUserId", /*user.id*/ "10");
 
-      // Login successfully worked --> navigate to the route /home in the HomeRouter
-      this.props.history.push(`/home`);
+      this.navigateToHomePage()
     } catch (error) {
-      alert(`Something went wrong during the login: \n${handleError(error)}`);
+      alert(`Something went wrong during the login process: \n${handleError(error)}`);
     }
   }
 
-
-  async register() {
-    this.props.history.push('/register');
+  navigateToRegisterPage() {
+    this.props.history.push("/register");
+  }
+  navigateToHomePage() {
+    this.props.history.push("/home");
   }
 
-  /**
-   *  Every time the user enters something in the input field, the state gets updated.
-   * @param key (the key of the state for identifying the field that needs to be updated)
-   * @param value (the value that gets assigned to the identified state key)
-   */
   handleInputChange(key, value) {
     // Example: if the key is username, this statement is the equivalent to the following one:
     // this.setState({'username': value});
     this.setState({ [key]: value });
   }
-
-  /**
-   * componentDidMount() is invoked immediately after a component is mounted (inserted into the tree).
-   * Initialization that requires DOM nodes should go here.
-   * If you need to load data from a remote endpoint, this is a good place to instantiate the network request.
-   * You may call setState() immediately in componentDidMount().
-   * It will trigger an extra rendering, but it will happen before the browser updates the screen.
-   */
   componentDidMount() {}
 
   render() {
@@ -104,30 +72,27 @@ class Login extends React.Component {
           <Form>
             <h1>Login</h1>
             <Form.Field>
-
               <Input
                 placeholder="Username"
-                onChange={e => {
-                  this.handleInputChange('username', e.target.value);
+                onChange={(e) => {
+                  this.handleInputChange("username", e.target.value);
                 }}
-                />
-              </Form.Field>
-       
-                <Form.Field>
-
-            <Input
-              type="password"
-              placeholder="Password"
-              onChange={e => {
-                this.handleInputChange('password', e.target.value);
-              }}
               />
-              </Form.Field>
-         
-         
+            </Form.Field>
+
+            <Form.Field>
+              <Input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => {
+                  this.handleInputChange("password", e.target.value);
+                }}
+              />
+            </Form.Field>
+
             <ButtonContainer>
               <Button
-              primary
+                primary
                 disabled={!this.state.username || !this.state.password}
                 width="50%"
                 onClick={() => {
@@ -136,13 +101,13 @@ class Login extends React.Component {
               >
                 Login
               </Button>
-              </ButtonContainer>
-              <ButtonContainer>
+            </ButtonContainer>
+            <ButtonContainer>
               <Button
                 secondary
                 width="50%"
                 onClick={() => {
-                  this.register();
+                  this.navigateToRegisterPage();
                 }}
               >
                 Don't have an account? Register here
@@ -154,10 +119,4 @@ class Login extends React.Component {
     );
   }
 }
-
-/**
- * You can get access to the history object's properties via the withRouter.
- * withRouter will pass updated match, location, and history props to the wrapped component whenever it renders.
- */
 export default withRouter(Login);
- 
