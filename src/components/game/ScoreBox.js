49a8d1dafd829fbb46442e7976277b4a2da30206
dio-git <60546759+dio-git@users.noreply.github.com>
2021-04-15 16:31:49 +0200

    #40 Redesigned end of round page

--- src/components/game/ScoreBox.js
+++ src/components/game/ScoreBox.js
@@ -1,56 +1,45 @@
 import React from 'react';
-import { Button, List, Grid, Progress, Form, Segment, Header } from 'semantic-ui-react';
+import { Button, List, Grid, Progress, Form, Segment, Header, Divider,Image } from 'semantic-ui-react';
 
 
 
 const ScoreBox = (props) => {
-    
-    const ListExampleDivided = () => (
+
+    const PlayerList = () => (
         <List divided verticalAlign='middle'>
-          <List.Item>
-            <List.Content floated='left'>
-              <List.Header as='a'>{props.players.player1.name}</List.Header>
-            </List.Content>
-            <List.Content floated='right'>
-                <Button>{props.players.player1.totalScore}</Button>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content floated='left'>
-              <List.Header as='a'>{props.players.player2.name}</List.Header>
-            </List.Content>
-            <List.Content floated='right'>
-                <Button>{props.players.player2.totalScore}</Button>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content floated='left'>
-              <List.Header as='a'>{props.players.player3.name}</List.Header>
-            </List.Content>
-            <List.Content floated='right'>
-                <Button>{props.players.player3.totalScore}</Button>
-            </List.Content>
-          </List.Item>
+            {props.scores.map((user) =>(
+              <List.Item>
+                <Image avatar floated='left' src='https://react.semantic-ui.com/images/avatar/small/matthew.png'/>
+                <List.Content floated='left'>
+                  <List.Header as='a'>{user.name}</List.Header>
+                  <List.Description>Total: {user.totalScore}</List.Description>
+                </List.Content>
+              <Button floated='right'>
+                <List.Description> {user.score}</List.Description>
+              </Button>
+              </List.Item>
+              
+            ))}
         </List>
-      )
+      );
 
     const ProgressBar = () => {
-        let color = props.players.player1.score > 2500?"green" : "red";
+        let color = props.scores[0].score > 2500?"green" : "red";
         return(
-        <Progress percent={props.players.player1.score/5000*100} color={color}>
-            <h3>{props.players.player1.score}/5000</h3>
+        <Progress percent={props.scores[0].score/5000*100} color={color}>
+            <h3>{props.scores[0].score}/5000</h3>
         </Progress>
         )}
     
     const NextRound= () =>{
       //TODO: get the lastRound flag from the gamecontroller
         let lastRound = false;
-        let button = 'Next round starts in : 00:00'
+        let button = 'Next round: 00:00'
         if(lastRound){
             button = 'EXIT';
         }
         return(
-            <Button animated='fade' color='teal' fluid size='large' onClick={()=>{props.nextRound()}}>
+            <Button animated='fade' color='teal' fluid size='large' onClick={()=>{props.nextQuestion()}}>
               <Button.Content visible>{button}</Button.Content>
               <Button.Content hidden>Go</Button.Content>
             </Button>
@@ -58,23 +47,36 @@ const ScoreBox = (props) => {
     }
 
     return ( 
-    
-    <Grid textAlign='center' style={{ height: '90vh' }} verticalAlign='middle'>
-        
+
+  <Segment placeholder raised>
+
+    <Header as='h2' color='teal' textAlign='center'>
+      <text>Your Score {props.scores[0].name}</text>
+      <ProgressBar/>
+      <Divider horizontal></Divider>
+    </Header>
+
+    <Grid columns={2} stackable textAlign='center'>
+
+      <Grid.Row verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as='h2' color='teal' textAlign='center'>
-                <text>Your Score {props.players.player1.name}</text>
-            </Header>
-            <Segment raised>
-                <Form size='large'>
-                    <ProgressBar/>
-                    <ListExampleDivided/>
-                    <NextRound/>
-                </Form>
-            </Segment>
+          <Form size='large'>
+            <PlayerList/>
+          </Form>
         </Grid.Column>
+
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Form size='large'>
+            <h1 style={{color:'black'}}>MINIMAP WITH DISTANCE</h1>
+          </Form>
+        </Grid.Column>
+      </Grid.Row>
+      
     </Grid>
-    )
+    <Divider horizontal> </Divider>
+    <NextRound/>
+  </Segment>
+)
 }
 
 export default ScoreBox;
